1493df1c64c75994a491bb991a573430000e9a78
DzygunRoman <dzygun-roman@mail.ru>
2024-11-27 13:19:00 +0300

    Initial commit

---
+++ app/src/main/java/com/hfad/kurs/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.hfad.kurs
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val userLogin: EditText = findViewById(R.id.user_login)
+        val userEmail: EditText = findViewById(R.id.user_email)
+        val userPass: EditText = findViewById(R.id.user_pass)
+        val button: Button = findViewById(R.id.button_reg)
+
+        button.setOnClickListener {
+            val login = userLogin.text.toString().trim()
+            val email = userEmail.text.toString().trim()
+            val pass = userPass.text.toString().trim()
+
+            if(login == "" || email == "" || pass == "")
+                Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
+            else {
+                val user = User(login, email, pass)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/hfad/kurs/User.kt
@@ -0,0 +1,4 @@
+package com.hfad.kurs
+
+class User(val login: String, val email: String, val pass: String) {
+}
