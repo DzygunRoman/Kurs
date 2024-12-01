c50367872453857e44e6825055ea818002c9d3ac
Roman Dzygun <dzygun-roman@mail.ru>
2024-12-01 12:05:08 +0300

    почти сделал

--- app/src/main/java/com/hfad/kurs/ItemsActivity.kt
+++ app/src/main/java/com/hfad/kurs/ItemsActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ItemsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,15 @@ class ItemsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val itemsList: RecyclerView = findViewById(R.id.itemsList)
+        val items = arrayListOf<Item>()
+
+        items.add(Item(1, "sofa", "Диван", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", "Sed do eiusmod tempor incididunt ut laboris nist ut aliguip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolor prodient, sunt in culpa aui officia deserunt mollit anim id est laborum.", 999))
+        items.add(Item(2, "light", "Свет", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", "Sed do eiusmod tempor incididunt ut laboris nist ut aliguip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolor prodient, sunt in culpa aui officia deserunt mollit anim id est laborum.", 399))
+        items.add(Item(3, "kitchen", "Кухня", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", "Sed do eiusmod tempor incididunt ut laboris nist ut aliguip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolor prodient, sunt in culpa aui officia deserunt mollit anim id est laborum.", 2999))
+
+        itemsList.layoutManager = LinearLayoutManager(this)
+        itemsList.adapter = ItemsAdapter(items, this)
     }
 }
